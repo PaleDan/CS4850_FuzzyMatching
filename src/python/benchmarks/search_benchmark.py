b10d263beb59e7ad5f6a1a0963c807f43727e59e
Aaron Roberts <aaronmroberts1@gmail.com>
2018-10-10 09:28:50 -0400

    update benchmarking for python and c benchmarking

--- src/python/benchmarks/search_benchmark.py
+++ src/python/benchmarks/search_benchmark.py
@@ -1,25 +1,14 @@
 import time
 import unittest
-from search import search
+from search import search_c
+from search import search_p
 
 
 class BenchmarkTests(unittest.TestCase):
 
-    def test_benchmark(self):
-        cases = [
-            {'str': 10, 'data': 1000000},
-            {'str': 10, 'data': 10000000},
-            {'str': 20, 'data': 1000000},
-            {'str': 20, 'data': 10000000},
-            {'str': 30, 'data': 1000000},
-            {'str': 30, 'data': 10000000},
-            {'str': 40, 'data': 1000000},
-            {'str': 40, 'data': 10000000},
-            {'str': 50, 'data': 1000000},
-            {'str': 50, 'data': 10000000},
-            {'str': 60, 'data': 1000000},
-            {'str': 60, 'data': 10000000},
-        ]
+    def test_search_c_benchmark(self):
+        print('search_c')
+        cases = []  # map(lambda x: {'str': x * x, 'data': 1000000}, range(10, 101))
 
         for case in cases:
             str_size = case['str']
@@ -28,16 +17,33 @@ class BenchmarkTests(unittest.TestCase):
             needle = 'A' * str_size
             haystack = list(map(lambda x: needle, range(1, data_size)))
 
-            begin = time.time()
-            search(needle, haystack)
-            end = time.time()
-            print(f"{str_size},{data_size} : {end - begin}")
+            begin = time.process_time()
+            search_c(needle, haystack)
+            end = time.process_time()
+            print(f"{str_size},{end - begin}")
+        self.assertEqual(1, 1)
+
+    def test_search_p_benchmark(self):
+        print('search_p')
+        cases = map(lambda x: {'str': x * x, 'data': 1000}, range(10, 21))
+
+        for case in cases:
+            str_size = case['str']
+            data_size = case['data']
+
+            needle = 'A' * str_size
+            haystack = list(map(lambda x: needle, range(1, data_size)))
+
+            begin = time.process_time()
+            search_p(needle, haystack)
+            end = time.process_time()
+            print(f"{str_size},{end - begin}")
         self.assertEqual(1, 1)
 
     def test_n_n_benchmark(self):
         cases = [
-            {'str': 10, 'data': 1000},
-            {'str': 10, 'data': 10000},
+            # {'str': 10, 'data': 1000},
+            # {'str': 10, 'data': 10000},
         ]
 
         for case in cases:
@@ -48,10 +54,10 @@ class BenchmarkTests(unittest.TestCase):
 
             begin = time.time()
             for needle in haystack:
-                search(needle, haystack)
+                search_c(needle, haystack)
             end = time.time()
             print(f"{str_size},{data_size} : {end - begin}")
-
+        self.assertEqual(1, 1)
 
 if __name__ == '__main__':
     unittest.main()
